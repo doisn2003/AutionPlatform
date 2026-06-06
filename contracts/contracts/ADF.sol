5007a9cf520891acb51b23f82d7b15cb1a20830c
doisn2003 <ngocdoisamsung@gmail.com>
2026-06-07 00:17:39 +0700

    third commit: publish tokens

--- contracts/contracts/ADF.sol
+++ contracts/contracts/ADF.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import {ERC20} from "./tokens-standard/ERC20.sol";
 
 contract ADF is ERC20 {
-    constructor() ERC20("Auction Data Format", "ADF") {
+    constructor() ERC20("Auction Decentralized Free", "ADF") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
-   }
+    }
 }
--- contracts/contracts/ADF_NFT.sol
+++ contracts/contracts/ADF_NFT.sol
@@ -4,10 +4,6 @@ pragma solidity ^0.8.23;
 import {ERC721} from "./tokens-standard/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ItemsNFT is ERC721, Ownable {
-    constructor(address initialOwner) 
-        ERC721("ItemsNFT", "ITEMS") 
-        Ownable(initialOwner) 
-    {
-    }  
+contract ADF_NFT is ERC721, Ownable {
+    constructor(address initialOwner) ERC721("Auction Decentralized Free - NFT", "ADFs") Ownable(initialOwner) {}
 }
